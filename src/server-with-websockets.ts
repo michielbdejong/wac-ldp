7fecec19e92eaad4881942ad7603bbd0fcb99720
Michiel de Jong <michiel@unhosted.org>
2020-01-13 15:30:40 +0100

    combine ldp-basic with rdf-fixtures

--- src/server-with-websockets.ts
+++ src/server-with-websockets.ts
@@ -51,7 +51,7 @@ export class Server {
 const port = parseInt((process.env.PORT ? process.env.PORT : ''), 10) || 8080
 
 const aud = process.env.AUD || 'https://localhost:8443'
-const server = new Server(port, aud, new URL(process.env.OWNER || 'https://alice.idp.test.solidproject.org/profile/card#me'))
+const server = new Server(port, aud, process.env.SKIP_WAC ? undefined : new URL('https://alice.idp.test.solidproject.org/profile/card#me'))
 server.listen().catch(console.error.bind(console))
 // server.close()
 
